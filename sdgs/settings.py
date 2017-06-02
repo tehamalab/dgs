e7b164da2620189dfedaf1c09e5902bad1e2c147
Emil <bmachaku@gmail.com>
2017-06-02 23:27:51 +0300

    add pagination

--- sdgs/settings.py
+++ sdgs/settings.py
@@ -165,5 +165,7 @@ ADMIN_INDEX_TITLE = os.environ.get('ADMIN_INDEX_TITLE', 'Management')
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
