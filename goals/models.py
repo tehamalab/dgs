18352be20b9553d7f63a658eb6fbf3a72de5ce4c
Emil <bmachaku@gmail.com>
2017-06-05 13:00:22 +0300

    add target_value to component

--- goals/models.py
+++ goals/models.py
@@ -171,6 +171,10 @@ class Component(models.Model):
     image_large = ImageSpecField(source='image',
                                  processors=[ResizeToFit(700)],
                                  options={'quality': 80})
+    target_value = models.FloatField(_('Target value'), blank=True,
+                                     null=True)
+    value_unit = models.CharField(_('Value unit'), blank=True,
+                                  max_length=50)
     slug = models.SlugField(_('Slug'), blank=True)
     created = models.DateTimeField(_('Created'), auto_now_add=True)
     last_modified = models.DateTimeField(_('Last modified'),
@@ -194,8 +198,6 @@ class Progress(models.Model):
                              related_name='progress')
     year = models.IntegerField(_('Year'))
     value = models.FloatField(_('Value'))
-    value_unit = models.CharField(_('Value unit'), blank=True,
-                                  max_length=50)
     created = models.DateTimeField(_('Created'), auto_now_add=True)
     last_modified = models.DateTimeField(_('Last modified'),
                                          auto_now=True)
@@ -215,6 +217,10 @@ class Progress(models.Model):
     @cached_property
     def component_name(self):
         return self.component.name
+
+    @cached_property
+    def value_unit(self):
+        return self.component.value_unit
 
     @cached_property
     def indicator_code(self):
