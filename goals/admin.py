16ca50639ac4012cfbef4cfce7a7124dee599530
Emil <bmachaku@gmail.com>
2017-07-31 19:26:25 +0300

    optimize admin site

--- goals/admin.py
+++ goals/admin.py
@@ -10,24 +10,27 @@ from .import_export import (AreaResource, GoalResource, TargetResource,
 class ProgressInline(admin.TabularInline):
     model = Progress
     exclude = ['slug', 'extras']
+    raw_id_fields = ['area']
 
 
 class ComponentInline(admin.TabularInline):
     model = Component.indicators.through
-    exclude = ['slug', 'extras']
+    raw_id_fields = ['component']
 
 
 class AreaTypeAdmin(ImportExportModelAdmin):
-    search_fields = ['code', 'name', 'description']
+    search_fields = ['code', 'name']
 
 
 class AreaAdmin(DraggableMPTTAdmin, ImportExportModelAdmin):
     resource_class = AreaResource
+    search_fields = ['code', 'name']
     prepopulated_fields = {"slug": ("name",)}
     list_filter = ['type']
 
 
 class PlanAdmin(ImportExportModelAdmin):
+    search_fields = ['^code', 'name']
     list_display = ['code', 'name', 'description']
     list_display_links = ['code', 'name']
     prepopulated_fields = {"slug": ("name",)}
@@ -35,51 +38,64 @@ class PlanAdmin(ImportExportModelAdmin):
 
 class GoalAdmin(ImportExportModelAdmin):
     resource_class = GoalResource
-    list_display = ['code', 'name', 'description']
-    list_display_links = ['code', 'name']
-    prepopulated_fields = {"slug": ("name",)}
     ordering = ['id']
+    search_fields = ['^code', 'name', 'description', 'plan__code',
+                     'plan__name']
+    list_display = ['plan_code', 'code', 'name', 'description']
+    list_display_links = ['code', 'name']
+    list_filter = ['plan']
+    prepopulated_fields = {"slug": ("name",)}
 
 
 class TargetAdmin(ImportExportModelAdmin):
     resource_class = TargetResource
-    list_display = ['code', 'description']
-    list_display_links = ['code', 'description']
-    list_filter = ['goal']
-    search_fields = ['code', 'description']
     ordering = ['id']
+    search_fields = ['^code', 'name', 'description']
+    list_display = ['plan_code', 'code', 'name']
+    list_display_links = ['code', 'name']
+    list_filter = ['goal__plan', 'goal']
 
 
 class IndicatorAdmin(ImportExportModelAdmin):
     resource_class = IndicatorResource
-    list_display = ['code', 'description']
-    list_display_links = ['code', 'description']
-    search_fields = ['code', 'description']
     ordering = ['id']
+    search_fields = ['^code', 'name', 'description', '=target__code']
+    list_display = ['plan_code', 'code', 'name']
+    list_display_links = ['code', 'name']
+    list_filter = ['target__goal__plan', 'target__goal']
+    raw_id_fields = ['target']
     inlines = [ComponentInline]
 
 
 class ComponentAdmin(ImportExportModelAdmin):
-    filter_horizontal = ['indicators']
+    ordering = ['id']
+    search_fields = ['^code', 'name', 'description', '^indicators__code']
     list_display = ['code', 'name']
     list_display_links = ['code', 'name']
-    list_filter = ['indicators__target__goal', 'stats_available',
-                   'agency', 'data_source']
+    list_filter = ['indicators__target__goal__plan',
+                   'indicators__target__goal', 'stats_available', 'agency',
+                   'data_source']
     prepopulated_fields = {"slug": ("name",)}
-    search_fields = ['code', 'name']
-    ordering = ['id']
+    filter_horizontal = ['indicators']
     inlines = [ProgressInline]
 
 
 class ProgressAdmin(ImportExportModelAdmin):
-    list_display = ['component_code', 'component_name', 'year', 'value',
-                    'value_unit']
-    list_display_links = ['component_name', 'year', 'value']
-    list_filter = ['year', 'fiscal_year', 'component__indicators']
-    search_fields = ['component__indicators__code',
-                     'component__indicators__description']
     ordering = ['id']
+    search_fields = ['component__name', '^component__code',
+                 '^component__indicators__code',
+                 'component__indicators__name']
+    list_display = ['component_code', 'component_name', 'groups', 'year',
+                    'value', 'value_unit']
+    list_display_links = ['component_name', 'year', 'value']
+    list_filter = [
+        'year',
+        'fiscal_year', 
+        'component__indicators__target__goal__plan',
+        'component__indicators__target__goal'
+    ]
     list_select_related = ['component']
+    raw_id_fields = ['component']
 
 
 admin.site.register(AreaType, AreaTypeAdmin)
